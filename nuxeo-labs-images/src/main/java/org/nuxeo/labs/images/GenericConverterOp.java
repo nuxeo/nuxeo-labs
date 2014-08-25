c79cd126801c326cfa70134c3c0383cc6d232ed6
Thibaud Arguillere <targuillere@nuxeo.com>
2014-08-24 23:32:58 -0400

    Add picture operations and crop toolbar button. Update readme

--- nuxeo-labs-images/src/main/java/org/nuxeo/labs/images/GenericConverterOp.java
+++ nuxeo-labs-images/src/main/java/org/nuxeo/labs/images/GenericConverterOp.java
@@ -1,7 +1,19 @@
-/**
- * 
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Frederic Vadon
  */
-
 package org.nuxeo.labs.images;
 
 import java.io.Serializable;
@@ -12,17 +24,12 @@ import java.util.Set;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.automation.core.Constants;
-import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 import org.nuxeo.ecm.automation.core.annotations.Param;
-import org.nuxeo.ecm.automation.core.collectors.DocumentModelCollector;
 import org.nuxeo.ecm.automation.core.collectors.BlobCollector;
 import org.nuxeo.ecm.automation.core.util.Properties;
 import org.nuxeo.ecm.core.api.Blob;
-import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.ecm.core.api.blobholder.BlobHolder;
 import org.nuxeo.ecm.core.api.blobholder.SimpleBlobHolder;
 import org.nuxeo.ecm.core.convert.api.ConversionService;
@@ -31,11 +38,13 @@ import org.nuxeo.runtime.api.Framework;
 /**
  * @author fvadon and aescaffre
  */
-@Operation(id = GenericConverter.ID, category = Constants.CAT_CONVERSION, label = "GenericConverter", description = "")
-public class GenericConverter {
-	
+@Operation(id = GenericConverterOp.ID, category = Constants.CAT_CONVERSION, label = "GenericConverter", description = "")
+public class GenericConverterOp {
+
+    public static final String ID = "GenericConverter";
+
 	public static final Log log = LogFactory
-			.getLog(GenericConverter.class);
+			.getLog(GenericConverterOp.class);
 
 	@Param(name = "converterName")
 	protected String converterName;
@@ -43,36 +52,34 @@ public class GenericConverter {
 	@Param(name = "parameters",  required = false)
 	protected Properties parameters;
 
-	public static final String ID = "GenericConverter";
-
 	@OperationMethod(collector = BlobCollector.class)
 	public Blob run(Blob input) {
 
 		try {
-			
+
 			ConversionService conversionService = Framework
 					.getService(ConversionService.class);
-		
+
 			BlobHolder source = new SimpleBlobHolder(input);
-			
+
 			Map<String, Serializable> serializableParameters = new HashMap<String, Serializable>();
 
 			if (parameters !=null) {
 				Set<String> parameterNames = parameters.keySet();
-			
+
 				for (String parameterName : parameterNames) {
 					serializableParameters.put(parameterName,
-							(Serializable) parameters.get(parameterName));
+							parameters.get(parameterName));
 				}
 			}
-			
+
 
 			log.debug("Converter Being called:"+converterName);
 			BlobHolder result = conversionService.convert(converterName,
 					source, serializableParameters);
-			
+
 			return result.getBlob();
-			
+
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
--- nuxeo-labs-images/src/main/java/org/nuxeo/labs/images/GenericImageMagickConverter.java
+++ nuxeo-labs-images/src/main/java/org/nuxeo/labs/images/GenericImageMagickConverter.java
@@ -1,3 +1,20 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Frederic Vadon
+ *     Alain Escaffre
+ */
 package org.nuxeo.labs.images;
 
 import java.io.Serializable;
@@ -11,6 +28,25 @@ import org.nuxeo.ecm.core.api.blobholder.BlobHolder;
 import org.nuxeo.ecm.core.convert.api.ConversionException;
 import org.nuxeo.ecm.platform.convert.plugins.PDF2ImageConverter;
 
+/**
+ *  In OSGI-INF/extensions/conversions-contrib.xml, you can find:
+ *      - The contribution to the ConversionService, using the
+ *        org.nuxeo.labs.images.GenericImageMagickConverter class
+ *      - And the contribution to the CommandLineExecutorComponent
+ *
+ *  So, to summarize, we have:
+ *      - Converters with unique names ("overlaying", "changeFormat", ...)
+ *      - Which are bound to commandLine ("imageOverlay", "changeFormat", ...)
+ *      - At runtime, the parameters are packed in a map and passed to the the
+ *        command line executor
+ *
+ *  IMPORTANT: This means the parameters must match what the command line expects.
+ *             For example, for "overlay", a parameter named "textSize" must
+ *             exist in the context, since the "convert" commandLine uses it
+ *             in its parameterString<> tag:
+ *                  . . . -pointsize #{textSize} . . .
+ *
+ */
 public class GenericImageMagickConverter extends PDF2ImageConverter {
 	public static final Log log = LogFactory
 			.getLog(GenericImageMagickConverter.class);
@@ -19,17 +55,18 @@ public class GenericImageMagickConverter extends PDF2ImageConverter {
 			Map<String, Serializable> parameters) throws ConversionException {
 		// TODO Auto-generated method stub
 		Map<String, String> cmdStringParameters= super.getCmdStringParameters(blobHolder, parameters);
-		
+
 		Map<String, String> stringParameters = new HashMap<String, String>();
 		Set<String> parameterNames = parameters.keySet();
 		for (String parameterName : parameterNames) {
 			//targetFilePath is computed by the method of the superType
-			if (!parameterName.equals("targetFilePath"))
-			stringParameters.put(parameterName,
-					(String) parameters.get(parameterName));
+			if (!parameterName.equals("targetFilePath")) {
+                stringParameters.put(parameterName,
+                		(String) parameters.get(parameterName));
+            }
 		}
 		cmdStringParameters.putAll(stringParameters);
 		return cmdStringParameters;
 	}
-	
+
 }
