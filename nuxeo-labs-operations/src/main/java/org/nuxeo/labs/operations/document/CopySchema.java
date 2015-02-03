dcfcba7f1ca95b49dd8599f1e84b2f353ad2fd2a
Fred Vadon <fvadon@nuxeo.com>
2015-02-03 11:54:09 -0500

    new operation to copy schema from one doc to another

---
+++ nuxeo-labs-operations/src/main/java/org/nuxeo/labs/operations/document/CopySchema.java
@@ -0,0 +1,49 @@
+package org.nuxeo.labs.operations.document;
+
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Context;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DataModel;
+import org.nuxeo.ecm.core.api.DocumentModel;
+
+@Operation(id=CopySchema.ID, category=Constants.CAT_DOCUMENT, label="Copy Schema", description="Copy all the info in the schema of the source to the input document.")
+public class CopySchema {
+
+    public static final String ID = "Document.CopySchema";
+
+    @Context
+    protected OperationContext context;
+
+    @Context
+    protected CoreSession session;
+
+    @Param(name = "source", required = false)
+    protected DocumentModel source;
+
+    @Param(name = "schema", required = true)
+    protected String schema;
+
+    @OperationMethod
+    public DocumentModel run(DocumentModel docToUpdate) {
+
+        if (source==null) {
+            source = (DocumentModel) context.get("request");
+        }
+
+        DataModel model = source.getDataModel(schema);
+        if (model!=null) {
+
+            DataModel targetDM = docToUpdate.getDataModel(schema);
+            if (targetDM!=null) {
+                // explicitly set values so that the dirty flags are set !
+                targetDM.setMap(model.getMap());
+            }
+        }
+
+        return docToUpdate;
+    }
+}
