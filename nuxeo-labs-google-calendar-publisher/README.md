1b0f5fb06c3e59447e6bf10f5113493560a8b6ac
Michael GENA <mgena@nuxeo.com>
2015-08-10 13:28:44 +0200

    Initial commit of the google-calendar-publisher plugin

---
+++ nuxeo-labs-google-calendar-publisher/README.md
@@ -0,0 +1,57 @@
+# nuxeo-labs-google-calendar-publisher
+This plugin enables you to publish on Google Calendar through a nuxeo automation operation.
+
+## Build
+
+Assuming [maven](http://maven.apache.org/) (3.2.1) is installed on your system, after downloading the whole repository, execute the following:
+```
+mvn install
+```
+
+## Deploy
+
+Upload the package marketplace generated under the folder nuxeo-google-calendar-publisher-mp/target/ into your local packages of the nuxeo platform and install it.
+
+## Nuxeo Studio
+
+Import the automation operation "Publisher" in your project registry. You will find a new entry under the automation tools:
+
+**Notification>Publish on Google Calendar**
+
+This operation expects multiple parameters:
+- userEmailAddress: the user e-mail to whom an event will be created
+- summary: the title of the event
+- location: the location of the event
+- description: the description of the event
+- startDate: the start date of the event (currently only full days events are handled)
+- endDate: the end date of the event
+- attendeeEmailAddress: e-mail address of the attendee (currently only one attendee is handled)
+
+## Configuration in the Nuxeo platform
+
+We use the OAuth2 authentification mechanism in order to do the publication on behalf of a user. In order to do that you need to have a google calendar api account with a client ID and Client Secret token. If you don't have one you have to create it here: https://console.developers.google.com/
+
+Then you need to add some modification under Administrator>Cloud Services>Service Providers>googledrive
+- Edit the Client ID and the Client Secret. Note that this is actually a workaround, the ideal solution would be to have a dedicated service provider named googlecalendar.
+
+- Edit the scope part by adding "email,https://www.google.com/calendar/feeds/" in order that the user authorizes the offline access to his calendar.
+
+## Usage
+
+Before launching the operation of publication, make sure that you go through the offline access authorization process, in order to have a valid token for the api. To do so, you need to go to Home>Cloud Services and Connect to Google Drive then accept the offline access to the user's calendar.
+That's it! Now you can run your publication operation, and you should be able to see that an event has been published on the calendar of the user.
+
+## Important Note
+
+**These features are not part of the Nuxeo Production platform.**
+
+These solutions are provided for inspiration and we encourage customers to use them as code samples and learning resources.
+
+This is a moving project (no API maintenance, no deprecation process, etc.) If any of these solutions are found to be useful for the Nuxeo Platform in general, they will be integrated directly into platform, not maintained here.
+
+
+## About Nuxeo
+
+Nuxeo provides a modular, extensible Java-based [open source software platform for enterprise content management](http://www.nuxeo.com/en/products/ep) and packaged applications for [document management](http://www.nuxeo.com/en/products/document-management), [digital asset management](http://www.nuxeo.com/en/products/dam) and [case management](http://www.nuxeo.com/en/products/case-management). Designed by developers for developers, the Nuxeo platform offers a modern architecture, a powerful plug-in model and extensive packaging capabilities for building content applications.
+
+More information at <http://www.nuxeo.com/>
---
+++ nuxeo-labs-google-calendar-publisher/src/main/java/org/nuxeo/google/calendar/Publisher.java
@@ -0,0 +1,133 @@
+/**
+ * 
+ */
+
+package org.nuxeo.google.calendar;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.GregorianCalendar;
+
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Context;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
+import org.nuxeo.ecm.automation.core.collectors.DocumentModelCollector;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.platform.oauth2.providers.OAuth2ServiceProvider;
+import org.nuxeo.ecm.platform.oauth2.providers.OAuth2ServiceProviderRegistry;
+import org.nuxeo.runtime.api.Framework;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
+
+/**
+ * @author Michael Gena
+ */
+@Operation(id=Publisher.ID, category=Constants.CAT_NOTIFICATION, label="Publish on Google Calendar", description="This operation lets you publish an event to a Google Calendar. The <code>userEmailAddress</code> is the e-mail address of the user who authorized an offline access to his calendar.")
+public class Publisher {
+
+    public static final String ID = "GoogleCalendarPublisher";
+    
+    @Context
+    protected OperationContext context;
+    
+    @Param(name = "userEmailAddress", required = true)
+    String userEmailAddress = "";
+
+    @Param(name = "summary", required = true)
+    String summary = "";
+    
+    @Param(name = "location", required = false)
+    String location = "";
+    
+    @Param(name = "description", required = false)
+    String description = "";
+    
+    @Param(name = "startDate", required = true)
+    GregorianCalendar startDate = new GregorianCalendar();
+    
+    @Param(name = "endDate", required = true)
+    GregorianCalendar endDate = new GregorianCalendar();
+    
+    @Param(name = "attendeeEmailAddress", required = false)
+    String attendeeEmailAddress = "";
+
+    @OperationMethod(collector=DocumentModelCollector.class)
+    public DocumentModel run(DocumentModel input) {
+		JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+		HttpTransport httpTransport;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+
+			//Credential credential = new GoogleCredential().setAccessToken(getAccessToken(userEmailAddress));
+			
+			OAuth2ServiceProvider serviceProvider = Framework.getLocalService(OAuth2ServiceProviderRegistry.class).getProvider("googledrive");
+	        Credential storedCredential = serviceProvider.loadCredential(userEmailAddress);
+			
+			Credential credential = new GoogleCredential.Builder()
+				    .setClientSecrets(serviceProvider.getClientId(), serviceProvider.getClientSecret())
+				    .setJsonFactory(JSON_FACTORY).setTransport(httpTransport).build()
+				    .setRefreshToken(storedCredential.getRefreshToken()).setAccessToken(storedCredential.getAccessToken());
+
+	    	// Initialize Calendar service with valid OAuth credentials	    	
+			Calendar service = new Calendar.Builder(httpTransport, JSON_FACTORY, credential).build();
+	    	// Create an Event and publish it
+	    	Event event = new Event()
+	    		    .setSummary(summary)
+	    		    .setLocation(location)
+	    		    .setDescription(description);
+	    	
+    		DateTime startDateTime = new DateTime(sdf.format(startDate.getTime()));
+    		EventDateTime start = new EventDateTime().setDate(startDateTime);
+    		event.setStart(start);
+    		
+       		//weird bug fix
+    		endDate.add(GregorianCalendar.DAY_OF_MONTH, 1);
+    		
+    		DateTime endDateTime = new DateTime(sdf.format(endDate.getTime()));
+    		EventDateTime end = new EventDateTime().setDate(endDateTime);
+    		event.setEnd(end);
+
+    		if(attendeeEmailAddress != null && !"".equals(attendeeEmailAddress)){
+	    		EventAttendee[] attendees = new EventAttendee[] {
+	    		    new EventAttendee().setEmail(attendeeEmailAddress)
+	    		};
+	    		event.setAttendees(Arrays.asList(attendees));
+    		}
+    		String calendarId = "primary";
+    		event = service.events().insert(calendarId, event).execute();	    	
+		} catch (GeneralSecurityException | IOException e) {
+			 throw new ClientException(e);
+		}		    	
+      return input; 
+    }  
+    
+    protected String getAccessToken(String user) {
+        OAuth2ServiceProvider serviceProvider = Framework.getLocalService(OAuth2ServiceProviderRegistry.class).getProvider("googledrive");
+        Credential credential = serviceProvider.loadCredential(user);
+        if (credential != null) {
+            String accessToken = credential.getAccessToken();
+            if (accessToken != null) {
+                return accessToken;
+            }
+        }
+        return null;
+    }
+ 
+}
