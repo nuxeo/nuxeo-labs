ab43d3a9942ae0ffad91ce699e1be81a8c980cda
Thibaud Arguillere <targuillere@nuxeo.com>
2015-10-09 14:30:49 +0200

    New AddFacet operation

---
+++ nuxeo-labs-operations/src/main/java/org/nuxeo/labs/operations/document/AddFacet.java
@@ -0,0 +1,50 @@
+/*
+ * (C) Copyright ${year} Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     thibaud
+ */
+
+package org.nuxeo.labs.operations.document;
+
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Context;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
+import org.nuxeo.ecm.automation.core.collectors.DocumentModelCollector;
+import org.nuxeo.ecm.automation.core.collectors.BlobCollector;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentRef;
+
+/**
+ * 
+ */
+@Operation(id=AddFacet.ID, category=Constants.CAT_DOCUMENT, label="Add Facet", description="Adds the facet to the document")
+public class AddFacet {
+
+    public static final String ID = "AddFacet";
+
+    @Param(name = "facet", required = true)
+    String facet = "";
+
+    @OperationMethod(collector=DocumentModelCollector.class)
+    public DocumentModel run(DocumentModel input) {
+        
+        boolean result = input.addFacet(facet);
+        
+        return input; 
+    }    
+
+}
